91e9a454e14fb52dfab7c58bf54edc0a6c60d025
Essonti <Strofim4enko@yandex.ru>
2021-11-05 16:37:41 +0300

    feat: add solution for "04-copy-directory" task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fsProm = require('fs/promises');
+const fs = require('fs');
+const path = require('path')
 
+try {
+    function copyDir () {
+    fsProm.mkdir('./04-copy-directory/file-copy', {recursive: true})
+    fs.readdir('./04-copy-directory/files', (err, files) => {
+        files.forEach(file => {
+            fsProm.copyFile(path.join('./04-copy-directory/files', file), path.join('./04-copy-directory/file-copy', file))
+        })
+    })
+}
+} catch {
+    console.log(`Can't be copied`)
+}
+copyDir()
