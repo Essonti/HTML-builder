c09dc5b9436f8ab477257234ec0fd0f08ae3c24b
Essonti <Strofim4enko@yandex.ru>
2021-11-09 22:26:23 +0300

    feat: implement deliting of files for task #04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,9 +2,17 @@ const fsProm = require('fs/promises');
 const fs = require('fs');
 const path = require('path')
 
+fs.readdir('./04-copy-directory/file-copy', (err, files) => {
+    files.forEach(file => {
+        fs.unlink(path.join('./04-copy-directory/file-copy', file), err => {
+            if (err) throw err;
+        })
+    })
+    });
 try {
     function copyDir () {
     fsProm.mkdir('./04-copy-directory/file-copy', {recursive: true})
+    
     fs.readdir('./04-copy-directory/files', (err, files) => {
         files.forEach(file => {
             fsProm.copyFile(path.join('./04-copy-directory/files', file), path.join('./04-copy-directory/file-copy', file))
